ffd5f537f48453dc0eb5b4e36f8874947b3036b8
Marsil Benavides <marsil.benavides@oxxo.com>
2023-07-30 14:29:36 -0600

    Removed version dependency

--- tomcat/build.cmd
+++ tomcat/build.cmd
@@ -1,2 +1,6 @@
 docker rmi "tomcat-10"
-docker build --no-cache . --tag "tomcat-10"
+docker build --no-cache^
+  --build-arg JDK_INSTALLER=jdk-17_linux-x64_bin.tar.gz^
+  --build-arg TOMCAT_INSTALLER=apache-tomcat-10.1.11.tar.gz^
+  --tag "tomcat-10"^
+  .
