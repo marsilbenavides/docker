835143633f3c6155d842bee8b6d36693d27a349e
Marsil Benavides <marsilbenavides@outlook.com>
2025-02-26 17:40:24 -0600

    Downgraded to 121300

--- jdev/build.sh
+++ jdev/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-docker rmi "oxxo-jdev:122100"
+docker rmi "oxxo-jdev:121300"
 docker build\
   --build-arg GROUP_ID=$(id -g ${USER})\
-  --build-arg JDEV_VERSION=12.2.1.0.0\
-  --build-arg JDEV_INSTALLER_1=jdev_suite_122100_linux64.bin\
-  --build-arg JDEV_INSTALLER_2=jdev_suite_122100_linux64-2.zip\
-  --build-arg RESPONSE_FILE=jdev_suite_122100_linux64.response\
+  --build-arg JDEV_VERSION=12.1.3.0.0\
+  --build-arg JDEV_INSTALLER=jdev_suite_121300_linux64.bin\
+  --build-arg RESPONSE_FILE=jdev_suite_121300_linux64.response\
   --build-arg JDK_INSTALLER=jdk-8u431-linux-x64.tar.gz\
-  --tag "oxxo-jdev:122100"\
+  --tag "oxxo-jdev:121300"\
   .
